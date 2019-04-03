2f8166a57589aafacc4c0397316e981f2a266e00
Den Raskovalov <denbox@gmail.com>
2019-04-03 16:48:54 -0700

    add data with KOIBs

--- russia2018/hist.py
+++ russia2018/hist.py
@@ -2,8 +2,17 @@
 
 import csv
 import matplotlib.pyplot as plt
+import json
+
+koibs = json.loads(open("koibs_kegs.json").read())
+sKoibs = set()
+for k, v in koibs.items():
+    for koib in v["koibs"]:
+        sKoibs.add(k + str(koib))
+print("#Koibs: %d" % len(sKoibs))
 
 putinBins = [0]*101
+putinKoibBins = [0]*101
 with open("table_227_level_3.tsv", "r") as f:
     next(f)
     for line in f:
@@ -11,6 +20,8 @@ with open("table_227_level_3.tsv", "r") as f:
         total = int(row[3])
         votes = int(row[12])
         putin = int(row[18])
+        region = row[0]
+        number = int(row[2][5:])
         # print(total, votes, putin)
         assert votes <= total
         assert putin <= votes
@@ -18,14 +29,18 @@ with open("table_227_level_3.tsv", "r") as f:
             turnover = float(votes)/total
             putinRatio = float(putin)/float(votes)
             putinBin = int(100.0 * putinRatio)
-            putinBins[putinBin] += putin
+            if region + str(number) in sKoibs:
+                putinKoibBins[putinBin] += putin
+            else:
+                putinBins[putinBin] += putin
 
 abins = []
 for i in range(101):
     abins.append(i)
 
 plt.clf()
-plt.plot(abins, putinBins, color='red', label="Putin")
+plt.plot(abins, putinBins, color='red', label="Putin without KOIB")
+plt.plot(abins, putinKoibBins, color='blue', label="Putin with KOIB")
 plt.legend()
 plt.suptitle("Russia")
 plt.savefig("pAll.png", dpi=300)
