fa7f93ab42406399b3d29af36ee80e20895c5e4b
Den Raskovalov <denbox@gmail.com>
2019-04-05 22:28:09 -0700

    plot more

--- russia2018/hist.py
+++ russia2018/hist.py
@@ -12,9 +12,12 @@ for k, v in koibs.items():
 print("#Koibs: %d" % len(sKoibs))
 
 putinBins = [0]*101
+nonPutinKoibBins = [0]*101
 putinKoibBins = [0]*101
 putinCrimeaBins = [0]*101
 putinCrimeaKoibBins = [0]*101
+regionTurnout = {}
+votes90 = 0
 with open("table_227_level_3.tsv", "r") as f:
     next(f)
     for line in f:
@@ -28,13 +31,22 @@ with open("table_227_level_3.tsv", "r") as f:
         assert votes <= total
         assert putin <= votes
         if votes:
-            turnover = float(votes)/total
+            turnoutPair = (votes, total)
+
+            turnout = float(votes)/total
             putinRatio = float(putin)/float(votes)
             putinBin = int(100.0 * putinRatio)
             if region + str(number) in sKoibs:
-                if putinBin > 89 and putinBin < 95:
+                if (putinBin > 89) and (putinBin <= 95) and (turnout < 0.65):
                     print(region, number, putin)
+                    if region in regionTurnout:
+                        regionTurnout[region] = (regionTurnout[region][0] + turnoutPair[0], regionTurnout[region][1] + turnoutPair[1])
+                    else:
+                        regionTurnout[region] = turnoutPair
+                    votes90 += votes
+
                 putinKoibBins[putinBin] += 3*putin
+                nonPutinKoibBins[putinBin] += 3*(votes - putin)
             else:
                 putinBins[putinBin] += putin
             if region == "Республика Крым":
@@ -43,9 +55,22 @@ with open("table_227_level_3.tsv", "r") as f:
                 else:
                     putinCrimeaBins[putinBin] += putin
 
+for r, pair in regionTurnout.items():
+    print("%s\t%f\t%d\t%f" % (r, pair[0]/pair[1], pair[0], float(pair[0])/votes90))
+
 abins = []
 for i in range(101):
     abins.append(i)
+
+plt.clf()
+plt.plot(abins, putinBins, color='red', label="Putin (KOIB only)")
+plt.plot(abins, nonPutinKoibBins, color='blue', label="Others (KOIB only)")
+plt.xlabel("Putin's votes share")
+plt.ylabel("Sum of #votes")
+plt.legend()
+plt.suptitle("Russia")
+plt.savefig("pOthers.png", dpi=300)
+plt.show()
 
 plt.clf()
 plt.plot(abins, putinBins, color='red', label="Putin without KOIB")
